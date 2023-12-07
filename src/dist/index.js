e752d774422d348856177921874002d5589715b3
Prabh Sagoo <prabhsagoo@yahoo.com>
2023-12-06 21:27:42 -0700

    MongoDB connection

--- src/dist/index.js
+++ src/dist/index.js
@@ -1,18 +1,13 @@
 import express from 'express';
-import Product  from './db/connection.js';
-
+import Product from './db/connection.js';
 const app = express();
-app.use(express.json())
-
-const PORT = 3001
-
+app.use(express.json());
+const PORT = 3001;
 app.get('/ping', (_req, res) => {
-    console.log('Someone has made a ping here')
-    res.send('pong!')
-})
-
+    console.log('Someone has made a ping here');
+    res.send('pong!');
+});
 app.get('/addProduct', (_req, res) => {
-   
     const product = new Product({
         name: 'Product 1',
         description: 'Description 1',
@@ -30,15 +25,13 @@ app.get('/addProduct', (_req, res) => {
                 price: 13.29
             }
         ]
-    })
-    product.save().then((result: any) => {
+    });
+    product.save().then((result) => {
         res.send(result);
-    }).catch((err: any) => {
+    }).catch((err) => {
         console.log(err);
     });
-    
-    })
-
+});
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
+    console.log(`Server is running on ${PORT}`);
+});
